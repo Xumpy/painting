146e360434e90bdcb95404f8ad6a85bdc6adb6b8
nmartens <nico.martens@dhl.com>
2020-10-07 15:34:30 +0200

    Finished fbi code

--- src/main/java/com/xumpy/painting/processes/builders/FrameLoader.java
+++ src/main/java/com/xumpy/painting/processes/builders/FrameLoader.java
@@ -6,7 +6,7 @@ public class FrameLoader extends ProcessStarter {
     private ProcessBuilder pb = new ProcessBuilder();
 
     public FrameLoader(String imageFile){
-
+        pb.command("sudo", "bash", "-c", "fbi -T 1 -d /dev/fb0 -noverbose -once " + imageFile);
     }
 
     @Override
